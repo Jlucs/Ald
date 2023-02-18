35ee15035383807c0381972e1ab119c9919e8003
Jlucs <janislucs@inbox.lv>
2023-02-18 23:26:22 +0200

    update

--- components/Dropzone/Dropzone.jsx
+++ components/Dropzone/Dropzone.jsx
@@ -16,7 +16,7 @@ export default function Dropzone({ open }) {
     <div className="container d-flex align-items-center justify-content-center flex-column mt-40">
       <div {...getRootProps({ className: "dropzone" })}>
         <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here</p>
+        <p>Drag and drop some files here</p>
         <div className="dropzone-icon">
             <svg width="48" height="48" viewBox="0 0 48 48"><path d="M20 8H8c-2.21 0-3.98 1.79-3.98 4L4 36c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4V16c0-2.21-1.79-4-4-4H24l-4-4z" fill="#bdbdbd"></path><path d="M0 0h48v48H0z" fill="none"></path></svg>
         </div>
